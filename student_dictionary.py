7df381a51c73bdcc092ce164df0e084cc5d03150
adhilakshmi11122007-eng <adhilakshmi11122007@gmail.com>
2026-09-16 08:24:19 +0530

    Add files via upload

---
+++ student_dictionary.py
@@ -0,0 +1,13 @@
+# Problem 2 - Dictionary
+
+student = {}
+
+student["name"] = input("Enter name: ")
+student["age"] = int(input("Enter age: "))
+student["course"] = input("Enter course: ")
+
+student["age"] = int(input("Enter updated age: "))
+student["course"] = input("Enter updated course: ")
+
+print("Updated Student Details:", student)
+print("Student details updated successfully!")
---
+++ tuple_marks.py
@@ -0,0 +1,9 @@
+# Problem 1 - Tuple
+
+marks = (80, 65, 90, 55, 75)
+
+high = max(marks)
+low = min(marks)
+
+print("Highest Mark:", high)
+print("Lowest Mark:", low)
